d87e2cd60d89ccd6614f9584e3cae030b1912daa
SamarathungaSASC <140165478+SamarathungaSASC@users.noreply.github.com>
2024-06-18 08:58:20 +0530

    Update Cost.scala

--- Cost.scala
+++ Cost.scala
@@ -1,22 +1,19 @@
-
-
 object Cost {
-  
-  def findCost(bookNum : Int, coverPrice: Double): Double = {
-    
-    if (bookNum > 50){
-      return (coverPrice*bookNum)+(3*50)+(0.75*(bookNum-50));
-    }
-    else{
-      return (coverPrice*bookNum)+(3*bookNum);
-    }
+  def bookCost(bookNum: Int):Double = bookNum *24.95;
+  def discount(price: Double):Double = price * 40/ 100;
+  def discountPrice(bookNum: Int): Double = {
+    val price = bookCost(bookNum)
+    price - discount(price)
   }
-  
+  def shipping(bookNum: Int):Double = {
+    if(bookNum <= 50)
+      3*50
+    else
+      3*50 + (bookNum - 50) * 0.75
+  }
+  def totalCost(bookNum: Int):Double = discountPrice(bookNum) + shipping(bookNum);
   def main(args: Array[String]){
-    
-    var coverPrice = 24.95 * (40.0/100.0);
-    var bookNum = 60;
-    var result = findCost(bookNum, coverPrice);
+    var result = totalCost(60);
     println(f"Total cost : $result%.2f Rs");
   }
 }
