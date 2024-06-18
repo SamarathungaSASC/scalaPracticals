ef058e63cb320a193f890408d3acdd9a928844a2
SamarathungaSASC <140165478+SamarathungaSASC@users.noreply.github.com>
2024-06-18 08:37:34 +0530

    Update Temperature.scala

--- Temperature.scala
+++ Temperature.scala
@@ -1,14 +1,6 @@
-
-
 object Temperature {
-  
-  def findTempInFahrenheit(c : Int): Double = {
-    
-    return c*1.8000 + 32.00;
-  }
-  
+  def findTempInFahrenheit(c : Int): Double = c*1.8000 + 32.00;
   def main(args: Array[String]){
-    
     var result = findTempInFahrenheit(35);
     println("Temperature in Fahrenheit : " + result + "°C");
   }
