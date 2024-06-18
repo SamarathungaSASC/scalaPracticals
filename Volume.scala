2687dad949912aa6b95e15c9800956c78c96f42a
SamarathungaSASC <140165478+SamarathungaSASC@users.noreply.github.com>
2024-06-18 10:37:57 +0530

    Update Volume.scala

--- Volume.scala
+++ Volume.scala
@@ -1,6 +1,6 @@
 object Volume {
    def findVolume(r : Int): Double = (4.0/3.0)* Math.PI *Math.pow(r,3) ;
-   def main(args: Array[String]){
+   def main(args: Array[String])={
     var result = findVolume(5);
     println(f"Volume of the sphere : $result%.2f");
   }
