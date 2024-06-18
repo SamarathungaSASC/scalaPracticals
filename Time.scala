b435ed86dfba8a29dcd39c68b764f7dde520519f
SamarathungaSASC <140165478+SamarathungaSASC@users.noreply.github.com>
2024-06-18 10:37:16 +0530

    Update Time.scala

--- Time.scala
+++ Time.scala
@@ -2,7 +2,7 @@ object Time {
   def easyTime(x: Int):Int = x*8;
   def tempoTime(y: Int):Int = y*7;
   def totalTime(d1: Int, d2: Int, d3: Int):Int = easyTime(d1) + tempoTime(d2) + easyTime(d3);
-  def main(args: Array[String]){
+  def main(args: Array[String])={
     var result = totalTime(2, 3, 2);
     println("Total time : " + result + " miniutes");
   }
