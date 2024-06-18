f084f8a137684e27146a2f5a623d64c9df00c8dc
SamarathungaSASC <140165478+SamarathungaSASC@users.noreply.github.com>
2024-06-18 10:34:50 +0530

    Update Area.scala

--- Area.scala
+++ Area.scala
@@ -1,7 +1,7 @@
 object Area {
   def calculateArea(r:Double):Double = Math.PI*r*r  
-  def main(args: Array[String]){
+  def main(args: Array[String])={
     var result = calculateArea(5);
-    println("Area : " + result);
+    println(f"Area :$result%.2f");
   }
 }
