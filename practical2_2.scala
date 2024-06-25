9ce5662fdf929fef75b3c6b4ec827473b42d4c81
SamarathungaSASC <140165478+SamarathungaSASC@users.noreply.github.com>
2024-06-25 08:24:35 +0530

    just add some comments to 2

--- practical2_2.scala
+++ practical2_2.scala
@@ -6,16 +6,16 @@ object expressionTwo{
     var K:Float =4.3f;
 
     def main(arg: Array[String])={
-        println((-b) * a + c * d);
-        println(a);
+        println((-b) * a + c * d);  // println( - -b * a + c *d - -)
+        println(a); //println(a++)
         a +=1;
         //println(-2*(g - k) + c);      error g is not dfine
-        println({
+        println({   // println (c=c++)
             val cNew = c;
             c+=1;
             cNew
         })
-        println({
+        println({   // println (c=++c*a++)
             c+=1;
             val result = c *a;
             a +=1;
